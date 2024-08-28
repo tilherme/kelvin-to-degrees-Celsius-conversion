5750293a4768d8832375bfe519ec2fd2bf2a636b
˜tilherme˜ <gui.mauricio@hotmail.com.br>
2024-08-28 11:55:17 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# kelvin-to-degrees-Celsius-conversion
---
+++ temp.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+const ebulicaoK = 373
+
+func main( ){
+	tempK := ebulicaoK
+	tempC := (tempK - 273)
+
+	fmt.Printf("A temperatura de ebulição da água em K = %d A temperatura de ebulição da água em C = %d ", tempK, tempC)
+
+
+
+
+}
